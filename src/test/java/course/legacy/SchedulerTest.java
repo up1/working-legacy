c0f86f8169917902f50820e1945eaecd763001c5
somkiat <somkiat.p@gmail.com>
2014-10-20 13:42:40 +0800

    Test timeservice

--- src/test/java/course/legacy/SchedulerTest.java
+++ src/test/java/course/legacy/SchedulerTest.java
@@ -12,12 +12,13 @@ import course.legacy.scheduler.Event;
 import course.legacy.scheduler.MailService;
 import course.legacy.scheduler.Meeting;
 import course.legacy.scheduler.Scheduler;
+import course.legacy.scheduler.TimeServices;
 
 class FakeDisplay implements Display {
 
 	public void showEvent(Event event) {
 	}
-	
+
 }
 
 class TestingScheduler extends Scheduler {
@@ -25,12 +26,11 @@ class TestingScheduler extends Scheduler {
 	public TestingScheduler(String owner, Display display) {
 		super(owner, display);
 	}
-	
+
 	protected void sendMail(String address, String subject, String message) {
 	}
-	
-}
 
+}
 
 public class SchedulerTest {
 
@@ -38,6 +38,18 @@ public class SchedulerTest {
 	public void createScheduler() {
 		Date now = new Date();
 		new TestingScheduler("somkiat", new FakeDisplay()).addEvent(new Event(now, DayTime.Time10AM));
+	}
+
+	@Test
+	public void returnMeetingWhenNotHoliday() throws Exception {
+		Date now = new Date();
+		Scheduler scheduler = new TestingScheduler("somkiat", new FakeDisplay());
+		scheduler.addEvent(new Meeting(now, DayTime.Time10AM, "For test"));
+		assertEquals("For test", scheduler.getMeeting(now, DayTime.Time10AM, new TimeServices() {
+			public boolean isDateAHoliday(Date date) {
+				return false;
+			}
+		}).getText());
 	}
 
 }
