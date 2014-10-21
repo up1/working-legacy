cb5738b288ef8aef56858008bec397dfcb4b605b
somkiat <somkiat.p@gmail.com>
2014-10-21 11:26:00 +0800

    My way to refactor code with SRP by create abstract class

---
+++ src/main/java/course/legacy/generalization/Command.java
@@ -0,0 +1,30 @@
+package course.legacy.generalization;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public abstract class Command {
+	
+	protected static final byte[] header = {(byte)0xde, (byte)0xad};
+	protected static final byte[] footer = {(byte)0xbe, (byte)0xef};
+	protected static final int SIZE_LENGTH = 1;
+	protected static final int CMD_BYTE_LENGTH = 1;
+	
+	public void write(OutputStream outputStream) throws Exception {
+		writeHeader(outputStream);
+		outputStream.write(getSize());
+		writeBody(outputStream);
+		writeFooter(outputStream);
+	}
+	public abstract int getSize();
+	public abstract void writeBody(OutputStream outputStream) throws Exception;
+	
+	private void writeFooter(OutputStream outputStream) throws IOException {
+		outputStream.write(footer);
+	}
+
+	private void writeHeader(OutputStream outputStream) throws IOException {
+		outputStream.write(header);
+	}
+
+}
--- src/main/java/course/legacy/generalization/LoginCommand.java
+++ src/main/java/course/legacy/generalization/LoginCommand.java
@@ -1,4 +1,5 @@
 package course.legacy.generalization;
+
 /*
  * Created by IntelliJ IDEA.
  * User: bbutton
@@ -8,39 +9,31 @@ package course.legacy.generalization;
  * Code Style | Class Templates options (Tools | IDE Options).
  */
 
+import java.io.IOException;
 import java.io.OutputStream;
 
-public class LoginCommand {
+public class LoginCommand extends Command {
 
-    private String userName;
-    private String passwd;
-    private static final byte[] header = {(byte)0xde, (byte)0xad};
-    private static final byte[] commandChar = {0x01};
-    private static final byte[] footer = {(byte)0xbe, (byte)0xef};
-    private static final int SIZE_LENGTH = 1;
-    private static final int CMD_BYTE_LENGTH = 1;
+	private String userName;
+	private String passwd;
 
-    public LoginCommand(String userName, String passwd) {
-        this.userName = userName;
-        this.passwd = passwd;
-    }
+	private static final byte[] commandChar = { 0x01 };
 
-    private int getSize() {
-        return header.length +  SIZE_LENGTH +  CMD_BYTE_LENGTH + footer.length +
-                userName.getBytes().length + 1 +
-                passwd.getBytes().length + 1;
-    }
+	public LoginCommand(String userName, String passwd) {
+		this.userName = userName;
+		this.passwd = passwd;
+	}
 
-    public void write(OutputStream outputStream) throws Exception {
-        outputStream.write(header);
-        outputStream.write(getSize());
-        outputStream.write(commandChar);
-        outputStream.write(userName.getBytes());
-        outputStream.write(0x00);
-        outputStream.write(passwd.getBytes());
-        outputStream.write(0x00);
-        outputStream.write(footer);
-    }
+	public int getSize() {
+		return header.length + SIZE_LENGTH + CMD_BYTE_LENGTH + footer.length + userName.getBytes().length + 1 + passwd.getBytes().length + 1;
+	}
+
+	public void writeBody(OutputStream outputStream) throws Exception {
+		outputStream.write(commandChar);
+		outputStream.write(userName.getBytes());
+		outputStream.write(0x00);
+		outputStream.write(passwd.getBytes());
+		outputStream.write(0x00);
+	}
+
 }
-
-
