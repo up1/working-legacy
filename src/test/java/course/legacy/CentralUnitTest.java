506951b2a87531081109c6525ec947071e634e54
somkiat <somkiat.p@gmail.com>
2014-10-21 15:26:11 +0800

    Add test case to remove null pointer exception (NPE)

--- src/test/java/course/legacy/CentralUnitTest.java
+++ src/test/java/course/legacy/CentralUnitTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import course.legacy.homeguard.AudibleAlarm;
@@ -15,59 +16,10 @@ import course.legacy.homeguard.Sensor;
 
 public class CentralUnitTest {
 
-	class FakeView implements HomeGuardView {
-
-		public void showMessage(String message) {
-		}
-
-	}
-
-	class FakeAudibleAlarm implements AudibleAlarm {
-
-		public int count = 0;
-
-		public void sound() {
-			count++;
-		}
-
-		public void silence() {
-		}
-
-	}
-
 	@Test
-	public void parseparseRadioBroadcastWithTRIPPED() throws Exception {
-		CentralUnit centralUnit = new CentralUnit();
-		centralUnit.registerSensor(new DoorSensor("1", "", Sensor.DOOR));
-		centralUnit.setView(new FakeView());
-		centralUnit.setAudibleAlarm(new FakeAudibleAlarm());
-
-		centralUnit.parseRadioBroadcast("1,TRIPPED");
-	}
-
-	@Test
-	public void parseparseRadioBroadcastWithTRIPPEDAndArm() throws Exception {
-		CentralUnit centralUnit = new CentralUnit();
-		centralUnit.registerSensor(new DoorSensor("1", "", Sensor.DOOR));
-		centralUnit.setView(new FakeView());
-		FakeAudibleAlarm alarm = new FakeAudibleAlarm();
-		centralUnit.setAudibleAlarm(alarm);
-		centralUnit.arm();
-
-		centralUnit.parseRadioBroadcast("1,TRIPPED");
-		assertTrue(alarm.count == 1);
-	}
-
-	@Test
-	public void parseparseRadioBroadcastWithTRIPPEDAndNotArm() throws Exception {
-		CentralUnit centralUnit = new CentralUnit();
-		centralUnit.registerSensor(new DoorSensor("1", "", Sensor.DOOR));
-		centralUnit.setView(new FakeView());
-		FakeAudibleAlarm alarm = new FakeAudibleAlarm();
-		centralUnit.setAudibleAlarm(alarm);
-
-		centralUnit.parseRadioBroadcast("1,TRIPPED");
-		assertTrue(alarm.count == 0);
+	public void testNPEToRemoveIfNull() throws Exception {
+		CentralUnit unit = new CentralUnit();
+		unit.parseRadioBroadcast("1,TRIPPED");
 	}
 
 }
