86a7e5102aeee54ad4428abb91c44d784126e9d3
somkiat <somkiat.p@gmail.com>
2014-10-21 09:24:40 +0800

    Refactor Long if-else with inheritance

---
+++ src/main/java/course/legacy/homeguard/DoorSensor.java
@@ -0,0 +1,13 @@
+package course.legacy.homeguard;
+
+public class DoorSensor extends Sensor {
+
+	public DoorSensor(String id, String location, String type) {
+		super(id, location, type);
+	}
+	
+	public String getType() {
+		return DOOR;
+	}
+
+}
--- src/test/java/course/legacy/SensorTest.java
+++ src/test/java/course/legacy/SensorTest.java
@@ -5,17 +5,25 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import course.legacy.homeguard.CentralUnit;
+import course.legacy.homeguard.DoorSensor;
 import course.legacy.homeguard.Sensor;
 
 public class SensorTest {
+	
+	private static Sensor makeSensor(String id, String location, String type) {
+		if(Sensor.DOOR.equals(type)) {
+			return new DoorSensor(id, location, type);
+		}
+		return new Sensor(id, location, type);
+	}
 
 	public static void assertSensorMessages(String type, String nonTrippedMessage, String trippedMessage) {
 		{
-			Sensor sensor = new Sensor("0", "<x>", type);
+			Sensor sensor = makeSensor("0", "<x>", type);
 			assertEquals(nonTrippedMessage, sensor.getMessage());
 		}
 		{
-			Sensor sensor = new Sensor("0", "<x>", type);
+			Sensor sensor = makeSensor("0", "<x>", type);
 			sensor.trip();
 			assertEquals(trippedMessage, sensor.getMessage());
 		}
@@ -23,10 +31,10 @@ public class SensorTest {
 
 	@Test
 	public void sensorMessage() throws Exception {
-		SensorTest.assertSensorMessages(Sensor.DOOR, "<x> is closed", "<x> is open");
-		SensorTest.assertSensorMessages(Sensor.WINDOW, "<x> is sealed", "<x> is ajar");
-		SensorTest.assertSensorMessages(Sensor.MOTION, "<x> is motionless", "Motion detected in <x>");
-		SensorTest.assertSensorMessages(Sensor.FIRE, "<x> temperature is normal", "<x> is on FIRE!");
+		assertSensorMessages(Sensor.DOOR, "<x> is closed", "<x> is open");
+		assertSensorMessages(Sensor.WINDOW, "<x> is sealed", "<x> is ajar");
+		assertSensorMessages(Sensor.MOTION, "<x> is motionless", "Motion detected in <x>");
+		assertSensorMessages(Sensor.FIRE, "<x> temperature is normal", "<x> is on FIRE!");
 	}
 
 }
