e32db4801e30c248e0f7ff9e81583eb64d954d6f
somkiat <somkiat.p@gmail.com>
2014-10-21 13:41:05 +0800

    finish

--- src/main/java/course/legacy/generalization/AddEmployeeCmd.java
+++ src/main/java/course/legacy/generalization/AddEmployeeCmd.java
@@ -1,15 +1,5 @@
 package course.legacy.generalization;
 
-/*
- * Created by IntelliJ IDEA.
- * User: bbutton
- * Date: Aug 1, 2002
- * Time: 9:34:33 AM
- * To change template for new class use 
- * Code Style | Class Templates options (Tools | IDE Options).
- */
-
-
 public class AddEmployeeCmd extends Command {
 
 	public AddEmployeeCmd(String name, String address, String city, String state, int yearlySalary) {
